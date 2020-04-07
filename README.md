aff7d523c94f5496d5ecbcc28918fb2de453a4d6
Digamber Singh <digamber@positronx.io>
2020-04-08 04:02:19 +0530

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vue-form-validation
 
+[Vue.js 2+ Forms Tutorial – Form Validation in Vue with Vuelidate](https://www.positronx.io/vue-js-forms-tutorial-form-validation-in-vue-with-vuelidate/)
+
 ## Project setup
 ```
 npm install
@@ -22,3 +24,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+# vue-form-validation
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormValidation></FormValidation>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FormValidation from '@/components/FormValidation.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    FormValidation
   }
 }
 </script>
